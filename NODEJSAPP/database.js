b2241bf2b48189a1fa7478a462fca8d57e3e6247
Yuan <zy21@uakron.edu>
2022-10-27 21:10:08 -0400

    add style

--- NODEJSAPP/database.js
+++ NODEJSAPP/database.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Qazwsx123,.",
     database: "programming_tutorial_youtube"
   });
 
