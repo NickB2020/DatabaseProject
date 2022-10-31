50291964b0b8f9924036af77a22d27e99a4f2104
Yuan <zy21@uakron.edu>
2022-10-30 21:56:38 -0400

    add get methods for nav menu on index view

--- NODEJSAPP/routes/web/home.js
+++ NODEJSAPP/routes/web/home.js
@@ -23,6 +23,66 @@ router.get("/home", function(req, res){
 
 });
 
+router.get("/cpp", function(req, res){
+
+  database.query("Select vid From video WHERE type = 'cpp' Limit 10", function (error, results) {
+    if(error) throw error;
+
+    res.render("home/", {data: results});
+  });
+
+});
+
+router.get("/java", function(req, res){
+
+  database.query("Select vid From video WHERE type = 'java' Limit 10", function (error, results) {
+    if(error) throw error;
+
+    res.render("home/", {data: results});
+  });
+
+});
+
+router.get("/javascript", function(req, res){
+
+  database.query("Select vid From video WHERE type = 'javascript' Limit 10", function (error, results) {
+    if(error) throw error;
+
+    res.render("home/", {data: results});
+  });
+
+});
+
+router.get("/sql", function(req, res){
+
+  database.query("Select vid From video WHERE type = 'sql' Limit 10", function (error, results) {
+    if(error) throw error;
+
+    res.render("home/", {data: results});
+  });
+
+});
+
+router.get("/asp", function(req, res){
+
+  database.query("Select vid From video WHERE type = 'asp' Limit 10", function (error, results) {
+    if(error) throw error;
+
+    res.render("home/", {data: results});
+  });
+
+});
+
+router.get("/python", function(req, res){
+
+  database.query("Select vid From video WHERE type = 'python' Limit 10", function (error, results) {
+    if(error) throw error;
+
+    res.render("home/", {data: results});
+  });
+
+});
+
 /* LOGIN PAGE */
 router.get("/login", function(req, res){
     res.render("home/login");
